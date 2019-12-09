295ba459c3f2de4f30f0dbb2a5d6b284b2c84a79
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2019-12-10 00:58:13 +0530

    Contact card done

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -91,4 +91,60 @@ class _HomePageState extends State<HomePage>{
     );
   }
 
+  Widget _buildListItem(BuildContext context, Record record){
+    return Card(
+      key: ValueKey(record.name),
+      elevation: 8.0,
+      margin: new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+      child: Container(
+        decoration: BoxDecoration(color: Color.fromRGBO(64,75, 96, 0.9)),
+        child: ListTile(
+          contentPadding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+          leading: Container(
+            padding: EdgeInsets.only(right: 12.0),
+            decoration: new BoxDecoration(
+              border: new Border(
+                right: new BorderSide(width: 1.0, color: Colors.white24)
+              )
+            ),
+            child: Hero(
+              tag: "avatar_" + record.name,
+              child: CircleAvatar(
+                radius: 32,
+                backgroundImage: NetworkImage(record.photo),
+              ),
+            ),
+          ),
+          title: Text(
+            record.name,
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)
+          ),
+          subtitle: Row(
+            children: <Widget>[
+              new Flexible(
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      RichText(
+                        text: TextSpan(
+                          text: record.address,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        maxLines: 3,
+                        softWrap: true,
+                      )
+                    ],
+                  )
+              )
+            ],
+          ),
+          trailing: Icon(
+            Icons.keyboard_arrow_right, color: Colors.white, size: 30.0,
+          ),
+          onTap: (){},
+        ),
+      ),
+    );
+  }
+
 }
