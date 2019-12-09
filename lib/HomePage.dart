82344c36cb5e9e30eebcf961e514fd047a58842a
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2019-12-10 00:21:11 +0530

    List search widget done

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -60,5 +60,35 @@ class _HomePageState extends State<HomePage>{
     );
   }
 
+  Widget _buildBar(BuildContext context){
+    return new AppBar(
+      elevation: 0.1,
+      backgroundColor: appDarkGreyColor,
+      centerTitle: true,
+      title: _appBarTitle,
+      leading: new IconButton(
+          icon: _searchIcon
+      ),
+    );
+  }
+
+
+  // handle the mapping of our RecordList data into our ListVew, and also handle any searches performed.
+  Widget _buildList(BuildContext context) {
+    if (!(_searchText.isEmpty)) {
+      _filteredRecords.records = new List();
+      for (int i = 0; i < _records.records.length; i++) {
+        if (_records.records[i].name.toLowerCase().contains(_searchText.toLowerCase())
+            || _records.records[i].address.toLowerCase().contains(_searchText.toLowerCase())) {
+          _filteredRecords.records.add(_records.records[i]);
+        }
+      }
+    }
+
+    return ListView(
+      padding: const EdgeInsets.only(top: 20.0),
+      children: this._filteredRecords.records.map((data) => _buildListItem(context, data)).toList(),
+    );
+  }
 
 }
